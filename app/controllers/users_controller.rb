d6860af1913bbb236217078354ffbe4f6764081b
Vinod Lala <vinod_lala@usa.net>
2013-12-09 09:51:20 -0500

    need to delete Scott

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,26 +45,26 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   # Don't let users do anything to other users
   def update
-    # respond_to do |format|
-    #   if @user.update(user_params)
-    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #     format.json { head :no_content }
-    #   else
-    #     format.html { render action: 'edit' }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   # Don't let users do anything to other users
   def destroy
-    # @user.destroy
-    # respond_to do |format|
-      # format.html { redirect_to users_url }
-      # format.json { head :no_content }
-    # end
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
   end
 
   private
